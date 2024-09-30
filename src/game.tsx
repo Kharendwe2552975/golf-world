70ace8cd7893b1b2926db35ea590da224c386eae
Kharendwe2552975 <2552975@students.wits.ac.za>
2024-09-30 11:38:41 +0200

    feat: added physics

--- src/game.tsx
+++ src/game.tsx
@@ -1,5 +1,28 @@
+import { Physics, useBox, usePlane } from '@react-three/cannon';
 import { Canvas } from '@react-three/fiber';
 import RotatingBox from './rotating-box';
+
+function Plane(props: any) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+  return (
+    // @ts-ignore
+    <mesh ref={ref}>
+      <planeGeometry args={[100, 100]} />
+    </mesh>
+  );
+}
+
+function Cube(props: any) {
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], ...props }));
+  return (
+    // @ts-ignore
+    <mesh ref={ref}>
+      <boxGeometry />
+      <meshStandardMaterial color={'hotpink'} />
+    </mesh>
+  );
+}
+
 const Game = () => {
   return (
     <Canvas>
@@ -8,6 +31,13 @@ const Game = () => {
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
       <RotatingBox position={[-1.2, 0, 0]} />
       <RotatingBox position={[1.2, 0, 0]} />
+
+      <Physics>
+        {/* <Debug color="black" scale={1.1}> */}
+        <Plane />
+        <Cube color="red" />
+        {/* </Debug> */}
+      </Physics>
     </Canvas>
   );
 };
