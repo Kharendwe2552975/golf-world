eb2523947d6a245d32ad2f7d0f350a84cac25cc9
Kharendwe2552975 <2552975@students.wits.ac.za>
2024-10-21 15:18:12 +0200

    feature: Added the hole and fixed the flag positioning

--- src/levels/one/grass.tsx
+++ src/levels/one/grass.tsx
@@ -1,7 +1,7 @@
-//@ts_nocheck
+//@ts-nocheck
 import { useBox, useCylinder } from '@react-three/cannon';
 import { shaderMaterial } from '@react-three/drei';
-import { extend } from '@react-three/fiber';
+import { extend, ReactThreeFiber } from '@react-three/fiber';
 import * as THREE from 'three';
 import { fragmentShader } from '../../components/shaders/fragmentShader';
 import { vertexShader } from '../../components/shaders/vertexShader';
@@ -24,7 +24,6 @@ extend({ CustomMaterial });
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      //@ts-ignore
       customMaterial: ReactThreeFiber.Object3DNode<
         THREE.ShaderMaterial,
         typeof THREE.ShaderMaterial
@@ -73,23 +72,21 @@ function GrassBlock({
 
   if (hasHole) {
     geometry = createTileWithHole(10, 3); // Create tile geometry with a hole
-    //@ts-ignore
-    // Physics cylinder for the hole to allow ball to fall through
+
     [ref] = useCylinder(() => ({
       args: [3, 3, 0.1, 32],
       position: [position[0], position[1], position[2]],
       type: 'Static',
       collisionFilterGroup: 0,
-    })) as [React.MutableRefObject<THREE.Mesh>];
+    })) as [React.MutableRefObject<THREE.Mesh>, any]; // Correctly destructure the tuple
   } else {
-    // Use box geometry for normal tiles
     geometry = new THREE.BoxGeometry(10, randomHeight, 10);
-    //@ts-ignore
+
     [ref] = useBox(() => ({
       args: [10, randomHeight, 10],
       position,
       type: 'Static',
-    })) as [React.MutableRefObject<THREE.Mesh>];
+    })) as [React.MutableRefObject<THREE.Mesh>, any]; // Correctly destructure the tuple
   }
 
   return (
@@ -103,9 +100,9 @@ function GrassBlock({
 // Main ground layout
 export default function GrassGround() {
   const tiles = [];
-  const numTilesX = 10; // Number of tiles horizontally
-  const numTilesZ = 20; // Number of tiles vertically
-  const holePosition = { x: 5, z: 2 }; // Block number 5 at the second row (z = 1)
+  const numTilesX = 10;
+  const numTilesZ = 20;
+  const holePosition = { x: 5, z: 2 }; // Block number 5 at the second row for where the hole is placed
 
   for (let x = 0; x < numTilesX; x++) {
     for (let z = 0; z < numTilesZ; z++) {
